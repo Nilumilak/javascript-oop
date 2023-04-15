71ecbe3f9dad4d3f93d1bdb72d561c8bc9eaed16
Renat Kalimulin <pianoparrot93@gmail.com>
2023-04-15 12:06:59 +0300

    OOP Tests

---
+++ src/Characters/Bowman.js
@@ -0,0 +1,7 @@
+import BaseCharacter from './BaseCharacter';
+
+export default class Bowman extends BaseCharacter {
+    constructor(name) {
+        super(name, 'Bowman', 25, 25);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './Characters/BaseCharacter';
+import './Characters/Bowman';
+import './Characters/Daemon';
+import './Characters/Magician';
+import './Characters/Swordsman';
+import './Characters/Undead';
+import './Characters/Zombie';
